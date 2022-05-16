e397f953c81416bccc0bdded4f4d572a3402115e
TJ Horner <me@tjhorner.com>
2022-05-16 19:09:28 -0400

    r 2

--- components/standing_desk_height/standing_desk_height.h
+++ components/standing_desk_height/standing_desk_height.h
@@ -4,8 +4,8 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
 
-#include "decoders/decoder.h"
-#include "decoders/jarvis_decoder.h"
+#include "decoder.h"
+#include "jarvis_decoder.h"
 
 namespace esphome {
 namespace standing_desk_height {
